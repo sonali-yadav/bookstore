877c6c2404f50bb0f06ae9468653b15a5ee90029
Sonali <simplysonali10053@gmail.com>
2018-09-05 15:42:42 +0530

    Added Book model,changed templates

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2, max_digits=8, default=0.0)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'store/index.html')
+
+
+def store(request):
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'store/store.html', context)
